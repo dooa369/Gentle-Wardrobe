766163f4607cec0cf81a554a79ee79967ec70b24
dooa369 <102269814+dooa369@users.noreply.github.com>
2023-09-25 16:21:44 +0900

    4일차

--- js/js.js
+++ js/js.js
@@ -8,8 +8,6 @@ loginButton.addEventListener("click", (event) => {
     const password = loginForm.password.value;
 
     if (username === "한정우" && password === "1234") {
-        alert("성공적으로 로그인되었습니다");
-        location.reload();
         location.href ="http://127.0.0.1:5500/%EB%A1%9C%EA%B7%B8%EC%9D%B8%20%EB%90%9C%20%ED%99%88%ED%8E%98%EC%9D%B4%EC%A7%80.html";
     } else {
         loginErrorMsg.style.opacity = 1;
