1159ff094aaa32692fd22cb32bacd7a63ac2be75
dooa369 <102269814+dooa369@users.noreply.github.com>
2023-09-26 23:12:57 +0900

    5일차

--- js/js.js
+++ js/js.js
@@ -7,7 +7,7 @@ loginButton.addEventListener("click", (event) => {
     const username = loginForm.username.value;
     const password = loginForm.password.value;
 
-    if (username === "한정우" && password === "1234") {
+    if (username === "홍길동" && password === "1234") {
         location.reload();
         location.href ="https://dooa369.github.io/Gentle-Wardrobe/%EB%A1%9C%EA%B7%B8%EC%9D%B8%20%EB%90%9C%20%ED%99%88%ED%8E%98%EC%9D%B4%EC%A7%80.html";
     } else {
